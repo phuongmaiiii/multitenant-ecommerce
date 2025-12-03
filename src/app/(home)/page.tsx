da443ad97062f2601aeea2e1db05b36978d03403
Phuong <phuong.nguyen@alumni.fh-aachen.de>
2025-12-03 18:16:15 +0100

    03-home-layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
@@ -5,7 +7,9 @@ import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
 
+
 export default function Home() {
+  
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
